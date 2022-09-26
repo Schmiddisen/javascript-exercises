c3d2c33af378660325ae3627e06850677c631727
Niklas <niklas.scmhmxdt@outlook.de>
2022-09-26 14:56:12 +0200

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  //convert to celsius and save in variable
+  convertedTemp = (temp - 32) * (5 / 9);
+  //round to one decimal place and return it
+  if (convertedTemp === 0) return parseFloat(convertedTemp);
+  return parseFloat(convertedTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  //convert to fahrenheit and save in variable
+  convertedTemp = temp * (9 / 5) + 32;
+  //round to one decimal place and return
+  if (convertedTemp === 0) return parseFloat(convertedTemp);
+  return parseFloat(convertedTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
