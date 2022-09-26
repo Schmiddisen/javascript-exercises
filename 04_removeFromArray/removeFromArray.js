8f4132afc4471112c1e10eeba81f9b907ec815ba
Niklas <niklas.scmhmxdt@outlook.de>
2022-09-26 13:16:38 +0200

    Solve removeFromArrays and reverseStrings

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+  for (let i = 0; i < array.length; i++) {
+    for (let o = 0; o < values.length; o++) {
+      if (array[i] === values[o]) {
+        array.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
