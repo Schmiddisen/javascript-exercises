6b91651506340cc29a4e9bc18701745ed2dde102
Niklas <niklas.scmhmxdt@outlook.de>
2022-09-26 14:43:57 +0200

    Solve sumAll and leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  //create variable added
+  let added = 0;
+  if (start > end) {
+    let temp = "";
+    temp = end;
+    end = start;
+    start = temp;
+  } else if (start < 0 || end < 0) {
+    return "ERROR";
+  } else if (typeof start !== "number" || typeof end !== "number"){
+    return "ERROR";
+  }
+  //create a loop between the two parameters
+  for (; start <= end; start++) {
+    //add the parameters and all numbers inbetween
+    added += start;
+  }
+  //return added
+  return added;
 };
 
 // Do not edit below this line
